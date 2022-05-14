8fe8f658d9f679a89c5f1ef47d2995381ca92746
avishekataur <avishekataur@gmail.com>
2022-05-14 20:48:28 +0600

    initial_ui_render_done

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log("Welcome to PORT", port);
+});
