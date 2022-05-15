e3c88b38185ad275e5aa5426a6f38030bc95f02b
avishekataur <avishekataur@gmail.com>
2022-05-15 21:57:32 +0600

    translate_normal

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 
-const app = express(); 
+const app = express();
 
 // Static Files
 app.use(express.static("public"));
 app.use("/js", express.static(__dirname + "public/js"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// app.use(express.urlencoded({ extended: true }));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -14,6 +18,11 @@ const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/translate/normal", (req, res) => {
+  let englishLine = req.body.name;
+  res.json({ name: englishLine });
 });
 
 app.listen(port, () => {
